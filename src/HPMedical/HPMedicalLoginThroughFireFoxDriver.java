9ac9bcb23b65e223fe6da96db995d53f43c307d6
103545 <103545@WIN10-49CMPH2.NTTDATAFED.COM>
2018-09-24 20:34:06 -0400

    test

---
+++ src/HPMedical/HPMedicalLoginThroughFireFoxDriver.java
@@ -0,0 +1,159 @@
+package HPMedical;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class HPMedicalLoginThroughFireFoxDriver {
+	
+	private static WebDriver driver;
+	private static Wait<WebDriver> wait;
+
+	
+	public static void main  (String [] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		//Here you will specify the driver type and its location 
+		System.setProperty("webdriver.gecko.driver","C:\\Selenium\\GeckoDriver\\geckodriver.exe");
+		
+		try {
+			
+			driver = new FirefoxDriver();
+			
+			//Maximize the browser			
+			driver.manage().window().maximize();
+			
+			wait = new WebDriverWait(driver, 40);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			
+			// Navigate to the application
+			driver.get("https://abac3.nttdatafed.com:8643/hpmedical");		
+			
+			
+			//Verify log in page title.
+			
+			String logInPageTitle = driver.getTitle();
+			System.out.println("The Log in Page title is :"+logInPageTitle);
+			
+			if (logInPageTitle.equalsIgnoreCase("HP Medical Login"))
+			{
+				System.out.println("Processed to the Login");
+				
+				Thread.sleep(3000); 
+				
+				WebElement uName = driver.findElement(By.id("username"));
+				
+				uName.sendKeys("rjohnson");
+				
+				WebElement passWord = driver.findElement(By.id("password"));
+				
+				passWord.sendKeys("P@ssword1");
+				
+			//	waitSeconds(2);
+				
+				WebElement signOn = driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/fieldset/table/tbody/tr[3]/td[2]/input"));
+				
+				signOn.click();
+				
+				//Home page verification and do implement test steps
+				
+				//NTTDATA title verification
+				// Richard id = 6
+				
+				String homePageUrl= driver.getCurrentUrl();
+				System.out.println("The homepae url is: "+homePageUrl);
+				
+				if(homePageUrl.equalsIgnoreCase("https://abac3.nttdatafed.com:8643/hpmedical/policies"))
+						{
+					
+					List<WebElement> policyTab = driver.findElements(By.xpath("/html/body/div[4]/table[1]//td"));
+					
+					int node_val = policyTab.size();
+					
+					System.out.println("The number of values are : "+node_val);
+					
+					// run a loop to find id 6 and Click
+					
+					for (int i=0; i< node_val; i++)
+					{
+						String policyTabVal = policyTab.get(i).getText();
+							
+								
+						System.out.println(policyTabVal);
+						
+						if (policyTabVal.equals("6"))
+								{
+							
+							Thread.sleep(4000);
+							policyTab.get(i).click();
+							break;							
+								}						
+						else
+						{
+							System.out.println("Not clicked");
+							driver.quit();
+						}
+					}					
+					
+						}
+				
+				Thread.sleep(4000);
+				
+				// Sign out the page
+				System.out.println("User is Signing out from the System");
+				
+				
+				driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/input")).click();
+				
+				Thread.sleep(4000);
+				
+				// Quit driver
+				
+				System.out.println("Now user quit driver");
+				driver.quit();
+				
+			}
+			
+			else {
+				System.out.println("The Log in Process is failed  --  The reason is title is not match with the expected result");
+				driver.quit();
+			}
+
+			
+			//Wait
+			
+			
+		}catch(Exception e)
+		{
+			System.out.println("Exception is :"+ e.getMessage());
+			
+		}
+		
+		
+		
+	}
+	
+	/*public void waitSeconds(int waitSec) throws Exception 
+	{
+		Thread.sleep(waitSec *1000);
+	}
+		
+	public void quitDriver()
+	{
+		driver.quit();
+	}*/
+	
+	
+	}
+
+
